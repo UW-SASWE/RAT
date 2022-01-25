e144db6dcd6d018e44457ae69f09be09d5c6153a
Pritam Das <pritamd47@gmail.com>
2022-01-25 11:18:46 -0800

    enable updatemeta and runrat

--- backend/scripts/run_rat.sh
+++ backend/scripts/run_rat.sh
@@ -8,9 +8,9 @@ source /houston2/pritam/activate_conda.sh
 conda activate /houston2/pritam/rat_mekong_v3/.condaenv
 echo "Activated Environment: "$CONDA_DEFAULT_ENV
 
-# python /houston2/pritam/rat_mekong_v3/backend/scripts/utils/update_meta.py
+python /houston2/pritam/rat_mekong_v3/backend/scripts/utils/update_meta.py
 
-# python /houston2/pritam/rat_mekong_v3/backend/scripts/run_rat.py
+python /houston2/pritam/rat_mekong_v3/backend/scripts/run_rat.py
 
 # PUBLISH
 echo "Publishing Results"
