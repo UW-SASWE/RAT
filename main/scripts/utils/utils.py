5defed1d06dcd0e5331683011e9d9d16d350b2af
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2022-05-12 13:41:24 -0700

    Initialized some scripts from pd_RAT_v2

---
+++ main/scripts/utils/utils.py
@@ -0,0 +1,38 @@
+import subprocess
+from logging import getLogger
+import os
+
+from utils.logging import LOG_NAME
+
+log = getLogger(LOG_NAME)
+
+
+
+def run_command(args, **kwargs):
+    """Safely runs a command, logs and returns the returncode silently in case of no error. 
+    Otherwise, raises an Exception
+    """
+    if isinstance(args, list):
+        log.debug("Running command: %s", " ".join(args))
+    else:
+        log.debug("Running command: %s", args)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kwargs)
+
+    with p.stdout:
+        for line in iter(p.stdout.readline, b''): # b'\n'-separated lines
+            log.debug("%r", line)
+        
+    exitcode = p.wait()
+
+    if exitcode == 0:
+        log.debug("Finished routing successfully: EXIT CODE %s", exitcode)
+    else:
+        log.error("ERROR Occurred with exit code: %s", exitcode)
+        raise Exception
+    
+    return exitcode
+
+def create_directory(p):
+    if not os.path.isdir(p):
+        os.makedirs(p)
+    return p
