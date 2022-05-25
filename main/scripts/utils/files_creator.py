36814b07de3da484ea8e1059d6db5ef6e79f5a44
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2022-05-24 19:21:48 -0700

    Added function to create vic soil_param and domain files for a basin

--- main/scripts/utils/files_creator.py
+++ main/scripts/utils/files_creator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import xarray as xr
 import rioxarray as rxr
 import rasterio
 from shapely.geometry import mapping
@@ -43,3 +45,25 @@ def create_basin_domain_nc_file(elevation_tif_filepath,basingridfile_path,output
     basin_domain['lon']=np.array([round_up(x,5) for x in basin_grid.lon.data ])
     #Saving dataset as netcdf
     basin_domain.to_netcdf(output_path)
+
+def create_vic_domain_param_file(global_vic_param_file,global_vic_domain_file,basingridfile_path,output_dir_path):
+    #Reading global files
+    gl_param = xr.open_dataset(global_vic_param_file)
+    gl_domain = xr.open_dataset(global_vic_domain_file)
+
+    #Reading basing grid file
+    basin_grid=rxr.open_rasterio(basingridfile_path)
+    basin_grid=basin_grid.drop_vars(['band','spatial_ref']).rename({'y':'lat','x':'lon'})[0]
+    basin_grid=basin_grid.fillna(0)
+
+    #Inserting run_cell as mask of basin_grid in vic_param.nc
+    basin_vic_param=gl_param.interp(lon=np.array([round_up(x,5) for x in basin_grid.lon.data ]),lat=np.array([round_up(x,5) for x in basin_grid.lat.data ]))
+    basin_vic_param['run_cell']=(('lat','lon'),basin_grid.data)
+    #Saving vic_param.nc
+    basin_vic_param.to_netcdf(os.path.join(output_dir_path,'vic_soil_param.nc'))
+
+    #Inserting run_cell as mask of basin_grid in vic_param.nc
+    basin_vic_domain=gl_domain.interp(lon=np.array([round_up(x,5) for x in basin_grid.lon.data ]),lat=np.array([round_up(x,5) for x in basin_grid.lat.data ]))
+    basin_vic_domain['mask']=(('lat','lon'),basin_grid.data)
+    #Saving vic_domain.nc
+    basin_vic_domain.to_netcdf(os.path.join(output_dir_path,'vic_domain.nc'))
