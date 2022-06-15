a095d3415ed174af86624e05974894710698ab32
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2022-06-15 11:38:57 -0700

    Initializing Remote Senssing scripts

---
+++ main/scripts/core/run_altimetry.py
@@ -0,0 +1,66 @@
+import configparser
+import os
+
+from data_processing import altimetry as alt
+
+
+def altimeter_routine(reservoirpath, j3tracks, user, password, metafile, project_dir, geoidpath):
+    resname = reservoirpath.split(os.sep)[-1].split('.')[0]
+
+    j3_pass = alt.get_j3_tracks(reservoirpath, j3tracks)
+    print(j3_pass)
+
+    if j3_pass is None:
+        return None
+
+    # savedir = os.path.join(project_dir, 'backend/data/altimetry', resname, 'raw')
+    savedir = os.path.join(project_dir, 'backend/data/altimetry', 'raw')
+    if not os.path.isdir(savedir):
+        os.makedirs(savedir)
+    # extracteddir = os.path.join(project_dir, 'backend/data/altimetry', resname, 'extracted')
+    extracteddir = os.path.join(project_dir, 'backend/data/altimetry', 'extracted', resname)
+    if not os.path.isdir(extracteddir):
+        os.makedirs(extracteddir)
+    
+    resultsdir = os.path.join(project_dir, 'backend/data/altimetry_timeseries')
+    savepath = os.path.join(resultsdir, f'{resname}.csv')
+
+    tracks = j3_pass['tracks']
+    lat_ranges = j3_pass['lat_range']
+
+    for track, lat_range in zip(tracks, lat_ranges):
+        latest_cycle = alt.get_latest_cycle(user, password, metafile)
+        print(f"Latest cycle: {latest_cycle}")
+
+        alt.download_data(user, password, savedir, track, 1, latest_cycle, 3)
+        extractedf = alt.extract_data(savedir + f'/j3_{track:03}', extracteddir, lat_range[0], lat_range[1], track, 3)
+
+        alt.generate_timeseries(extractedf, savepath, lat_range[0], lat_range[1], geoidpath)
+
+def run_altimetry(project_dir):
+    reservoirs_dir = os.path.join(project_dir, 'backend/data/ancillary/reservoirs')
+
+    to_process = ['7003', 'Xe_Kaman_1', 'Siridhorn', '5796', 'Lam_Pao']
+
+    reservoirs = [os.path.join(reservoirs_dir, f) for f in os.listdir(reservoirs_dir) if f.endswith(".shp") and f.split('.')[0] in to_process]
+
+    secrets = configparser.ConfigParser()
+    secrets.read(os.path.join(project_dir, 'backend/params/secrets.ini'))
+    username = secrets["aviso"]["username"]
+    pwd = secrets["aviso"]["pwd"]
+
+    metafile = os.path.join(project_dir, 'backend/params/j3_meta.txt')
+    
+    j3_tracks = os.path.join(project_dir, 'backend/data/ancillary/j3_tracks.geojson')
+
+    geoidpath = os.path.join(project_dir, 'backend/data/ancillary/geoidegm2008grid.mat')
+
+    for reservoir in reservoirs:
+        print(f"Processing {reservoir}")
+        # if reservoir.split('/')[-1].startswith("Siri"):
+        altimeter_routine(reservoir, j3_tracks, username, pwd, metafile, project_dir, geoidpath)
+
+
+
+if __name__ == '__main__':
+    run_altimetry("/houston2/pritam/rat_mekong_v3")
---
+++ main/scripts/core/run_sarea.py
@@ -0,0 +1,36 @@
+import os
+import datetime
+import subprocess
+
+from logging import getLogger
+from utils.logging import LOG_NAME, NOTIFICATION
+from utils.utils import run_command
+
+log = getLogger(f"{LOG_NAME}.{__name__}")
+
+def run_sarea(end_date):
+    reservoirs = [
+        "Battambang_1", "Lam_Pao", "Lower_Sesan_2", "Nam_Ngum_1", "Phumi_Svay_Chrum", "Sesan_4", 
+        "Sirindhorn", "Sre_Pok_4", "Ubol_Ratana", "Xe_Kaman_1","Yali", "5117", "5138", "5143",
+        "5147", "5148", "5151", "5152", "5155", "5156", "5160", "5162", "5795", "5797", "7000",
+        "7001", "7002", "7004", "7037", "7087", "7158", "7159", "7164", "7181", "7201", "7232",
+        "7284", "7303", "Nam_Theun_2", "Phumi_Svay_Chrum"#, "Nam_Ton"
+    ]
+    start_date = "2013-01-01"
+    # end_date = datetime.datetime.today().strftime("%Y-%m-%d")
+    script_path = "/houston2/pritam/rat_mekong_v3/backend/scripts/core/sarea_cli.py"
+
+    for reservoir in reservoirs:
+        arg = f"python {script_path} {reservoir} {start_date} {end_date}".split()
+
+        log.debug(f"Reservoir: {reservoir}; Running command: {arg}")
+
+        print(subprocess.run(arg, stdout=subprocess.PIPE), flush=True)
+
+
+def main():
+    run_sarea()
+
+
+if __name__ == '__main__':
+    main()
