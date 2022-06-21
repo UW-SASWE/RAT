9ee14580474f60b07b856421ccfc75c6b1f54b6e
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2022-06-21 13:40:41 -0700

    Earth engine credential template

---
+++ main/scripts/ee_utils/ee_config_template.py
@@ -0,0 +1,5 @@
+# Required credentials to authorize earth engine #
+service_account = "...... Add your service account here ......"
+key_file= 'Path to private json key file'
+
+##### After adding the account name and path to the key file rename this file to ee_config.py ####
