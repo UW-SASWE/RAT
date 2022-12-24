0c818cebd6ec7ba44846f3b44a048b3dddad1bee
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2022-12-24 15:12:50 -0800

    Combined nc file will only have data of latest 120 days and the period to run

--- src/rat/data_processing/metsim_input_processing.py
+++ src/rat/data_processing/metsim_input_processing.py
@@ -146,9 +146,10 @@ class CombinedNC:
             existing.close()
             last_existing_time = existing.time[-1]
             log.debug("Existing data: %s", last_existing_time)
+            existing_to_append = existing.sel(time=slice(last_existing_time - np.timedelta64(120,'D') , last_existing_time))
             ds = ds.sel(time=slice(last_existing_time + np.timedelta64(1,'D') , ds.time[-1]))
             # ds = ds.isel(time=slice(1, None))
-            xr.merge([existing, ds]).to_netcdf(self._outputpath)
+            xr.merge([existing_to_append, ds]).to_netcdf(self._outputpath)
         else:
             log.debug(f"Creating new file at {self._outputpath}")
             ds.to_netcdf(self._outputpath)
