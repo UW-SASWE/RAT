ec7bfcd0d2a85a0d5338fe0b52af528b64334541
Pritam Das <pritamd47@gmail.com>
2023-01-16 12:59:34 -0800

    added init and run options

--- src/rat/cli/rat_cli.py
+++ src/rat/cli/rat_cli.py
@@ -1,16 +1,48 @@
 import argparse
 
-from rat.run_rat import run_rat
 
+def init_func(args):
+    print("Initializing RAT using: ", args) # TODO: debug line, delete later
+
+def run_func(args):
+    print("Running RAT using: ", args) # TODO: debug line, delete later
 
 def main():
-    parser = argparse.ArgumentParser(description='Reservoir Assessment Tool 2.1')
-
-    parser.add_argument('input', action='store', nargs=1)
+    ## CLI interface
+    p = argparse.ArgumentParser(description='Reservoir Assessment Tool')
     
-    args = parser.parse_args()
+    # Treat the different commands, such as `init`, and `run` as different sub parsers
+    command_parsers = p.add_subparsers()
 
-    print(args.input)
+    # Init command
+    init_parser = command_parsers.add_parser('init', help='Initialize RAT')
+
+    # options for init parser
+    init_parser.add_argument(
+        '-d', '--dir', 
+        help='Specify RAT project directory', 
+        action='store',
+        dest='project_dir',
+        required=False
+    )
+
+    init_parser.set_defaults(func=init_func)
+
+    # Run command
+    run_parser = command_parsers.add_parser('run', help='Run RAT')
+
+    run_parser.add_argument(
+        '-p', '--param',
+        help='RAT Parameter file',
+        action='store',
+        dest='param',
+        required=True
+    )
+    
+    run_parser.set_defaults(func=run_func)
+
+    print(p.parse_args(['init', '-d', 'test_directory']))
+    print(p.parse_args(['run', '-p', 'test_param']))
 
 
 if __name__ == '__main__':
