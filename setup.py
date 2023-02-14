160e7b256285110caed422142c6155d2015ec95c
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2023-02-14 03:25:48 -0800

    Updated version & dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rat",
-    version = "2.1",
+    version = "3.0.0.alpha.1",
     license = "GPL-3.0",
     package_dir = {"": "src"}
 )
