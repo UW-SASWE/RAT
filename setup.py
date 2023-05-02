50e4188250089c017dcb1b2ebfbf048537266af0
Pritam Das <pritamd47@gmail.com>
2023-05-02 19:45:44 +0000

    update setup.py with placeholer version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rat",
-    version = "3.0.0.alpha.4",
+    version = "develop",
     license = "GPL-3.0",
     package_dir = {"": "src"}
 )
