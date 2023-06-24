4236037817f1e737da07d6fa777a70d16715cb14
Sanchit Minocha <sanchitminochaiitr@gmail.com>
2023-06-23 17:09:20 -0700

    Added print messages, configure command user specified global data directory to avoid memory limitation.

--- src/rat/cli/rat_init_config.py
+++ src/rat/cli/rat_init_config.py
@@ -7,42 +7,26 @@ DOWNLOAD_LINKS = {
 
 SUFFIXES_GLOBAL = {
     'GLOBAL': {
-        'project_dir': '',
-        'data_dir': 'data',
-        'basin_shpfile': 'global_data/global_basin_data/shapefiles/mrb_basins.json',
-        'elevation_tif_file': 'global_data/global_elevation_data/World_e-Atlas-UCSD_SRTM30-plus_v8.tif',
-        'basins_metadata': 'params/basins_metadata.csv'
-    },
-
-    'METSIM':{
-        'metsim_env': 'models/metsim',
-        'metsim_param_file': 'params/metsim/params.yaml'
+        'basin_shpfile': 'global_basin_data/shapefiles/mrb_basins.json',
+        'elevation_tif_file': 'global_elevation_data/World_e-Atlas-UCSD_SRTM30-plus_v8.tif',
     },
 
     'VIC': {
-        'vic_env': 'models/vic',
-        'vic_param_file': 'params/vic/vic_params.txt',
-        'vic_global_param_dir': 'global_data/global_vic_params'
+        'vic_global_param_dir': 'global_vic_params'
     },
 
     'ROUTING': {
-        'route_model': 'models/routing/rout',
-        'route_param_file': 'params/routing/route_param.txt',
-        'global_flow_dir_tif_file': 'global_data/global_drt_flow_file/global_drt_flow_16th.tif',
-        'stations_vector_file': 'global_data/global_dam_data/GRanD_dams_v1_3_filtered.shp'
-    },
-
-    'ROUTING PARAMETERS':{
-        'uh': 'params/routing/uh.txt'
+        'global_flow_dir_tif_file': 'global_drt_flow_file/global_drt_flow_16th.tif',
+        'stations_vector_file': 'global_dam_data/GRanD_dams_v1_3_filtered.shp'
     },
 
     'GEE': {
-        'reservoir_vector_file': 'global_data/global_reservoir_data/GRanD_reservoirs_v1_3.shp'
+        'reservoir_vector_file': 'global_reservoir_data/GRanD_reservoirs_v1_3.shp'
     },
 
     'ALTIMETER': {
-        'altimeter_tracks': 'global_data/global_altimetry/j3_tracks.geojson',
-        'geoid_grid': 'global_data/global_altimetry/geoidegm2008grid.mat'
+        'altimeter_tracks': 'global_altimetry/j3_tracks.geojson',
+        'geoid_grid': 'global_altimetry/geoidegm2008grid.mat'
     },
 }
 
